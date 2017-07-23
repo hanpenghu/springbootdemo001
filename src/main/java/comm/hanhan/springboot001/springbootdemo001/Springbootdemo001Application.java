6eeee4f72cbd8c0e3964d8ba1837d72c36380859
hanpenghu <563909408@qq.com>
2017-07-23 21:03:10 +0800

    2017_7_23   weekday(0)   21:03:04

--- src/main/java/comm/hanhan/springboot001/springbootdemo001/Springbootdemo001Application.java
+++ src/main/java/comm/hanhan/springboot001/springbootdemo001/Springbootdemo001Application.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication//写上这个注解是为了自动扫描该文件下所有的包
 //下面一个注解是扫描接口完成dao层注入
-@MapperScan("com.hanhan.springboot001.springbootdemo001.dao.mapperJava01")
+@MapperScan("comm.hanhan.springboot001.springbootdemo001.dao.mapperJava01")
 public class Springbootdemo001Application {
 
 	public static void main(String[] args) {
