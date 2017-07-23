85f96c1d647050f60d40ccda985231051166859b
hanpenghu <563909408@qq.com>
2017-07-23 12:37:38 +0800

    2017_7_23   weekday(0)   12:37:34

--- src/main/java/com/hanhan/springboot001/springbootdemo001/controller/LoginController.java
+++ src/main/java/com/hanhan/springboot001/springbootdemo001/controller/LoginController.java
@@ -1,7 +1,5 @@
 package com.hanhan.springboot001.springbootdemo001.controller;
 
-import org.springframework.http.HttpRequest;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
